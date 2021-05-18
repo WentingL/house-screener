73308c27a4978403aac99758f30beb6dc23bcefa
WentingL <kimluo2993@gmail.com>
2021-05-17 19:31:17 -0700

    test(db-wrapper): add unit test

--- house_screener/db/prepared_statement.py
+++ house_screener/db/prepared_statement.py
@@ -3,11 +3,13 @@ class PreparedStatementBuffer:
 
     def __init__(self, table):
         self.table = table
+        self.__str_list = []
 
     def select(self, *args):
         self.__str_list.append("SELECT ")
         self.__append_fields(*args)
         self.__str_list.append(" ")
+        return self
 
     def update(self, fields, values):
         self.__str_list.append("UPDATE {} SET (".format(self.table))
@@ -15,9 +17,11 @@ class PreparedStatementBuffer:
         self.__str_list.append(") VALUES (")
         self.__append_fields(values)
         self.__str_list.append(") ")
+        return self
 
     def delete(self, condition):
         self.__str_list.append("DELETE FROM {} WHERE {} ".format(self.table, condition))
+        return self
 
     def insert(self, fields, values):
         self.__str_list.append("INSERT INTO {} (".format(self.table))
@@ -25,18 +29,17 @@ class PreparedStatementBuffer:
         self.__str_list.append(") VALUES (")
         self.__append_fields(values)
         self.__str_list.append(") ")
+        return self
 
     def where(self, condition):
         """the condition should be in the format of 'field = value' or 'field like \'value\'"""
         self.__str_list.append("WHERE ")
         self.__str_list.append(condition)
         self.__str_list.append(" ")
+        return self
 
     def __append_fields(self, fields):
-        for f in fields:
-            self.__str_list.append(f)
-            self.__str_list.append(", ")
-        self.__str_list.pop(-1)
+        self.__str_list.append(', '.join(fields))
 
     def build(self) -> str:
         return ''.join(self.__str_list)
--- house_screener/test/test_preparedStatementBuffer.py
+++ house_screener/test/test_preparedStatementBuffer.py
@@ -1,5 +1,46 @@
-from unittest import TestCase
+import unittest
+
+from house_screener.db.prepared_statement import PreparedStatementBuffer
 
 
-class TestPreparedStatementBuffer(TestCase):
-    pass
+class TestPreparedStatementBuffer(unittest.TestCase):
+    testStmt: PreparedStatementBuffer
+    table = "test_table"
+
+    def setUp(self) -> None:
+        self.testStmt = PreparedStatementBuffer(self.table)
+
+    def tearDown(self) -> None:
+        self.testStmt: PreparedStatementBuffer
+
+    def test_select(self):
+        fields = ["test_field1", "test_field2", "test_field3", ]
+        self.assertEqual("SELECT {}, {}, {} ".format(fields[0], fields[1], fields[2]),
+                         self.testStmt.select(fields).build())
+
+    def test_update(self):
+        fields = ["test_field1", "test_field2", "test_field3", ]
+        values = ["test_value1", "test_value2", "test_value3", ]
+        self.assertEqual(
+            "UPDATE {} SET ({}, {}, {}) VALUES ({}, {}, {}) ".format(self.table, fields[0], fields[1], fields[2],
+                                                                     values[0], values[1], values[2]),
+            self.testStmt.update(fields, values).build())
+
+    def test_delete(self):
+        self.assertEqual("DELETE FROM test_table WHERE test = condition ",
+                         self.testStmt.delete("test = condition").build())
+
+    def test_insert(self):
+        fields = ["test_field1", "test_field2", "test_field3", ]
+        values = ["test_value1", "test_value2", "test_value3", ]
+        self.assertEqual(
+            "INSERT INTO {} ({}, {}, {}) VALUES ({}, {}, {}) ".format(self.table, fields[0], fields[1], fields[2],
+                                                                      values[0], values[1], values[2]),
+            self.testStmt.insert(fields, values).build())
+
+    def test_where(self):
+        self.assertEqual("WHERE test = condition ", self.testStmt.where("test = condition").build())
+
+
+if __name__ == '__main__':
+    unittest.main()
