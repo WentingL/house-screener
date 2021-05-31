9af9b822bec43c3bfadd9903d5a130816db25559
WentingL <kimluo2993@gmail.com>
2021-05-31 07:24:21 -0700

    feat(db-connection): initial module structure

---
+++ house_screener/db_connection.py
@@ -0,0 +1,12 @@
+"""
+pull data from real estate api to the local database
+provide methods for pulling analyzable data from databsae
+"""
+
+
+class Updater:
+    pass
+
+
+class DbConnection:
+    pass
